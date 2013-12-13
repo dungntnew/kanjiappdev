7fd52072f20477a4ab8fb4a642525d09e67325f7
Dung Nguyen <dung-nguyen@kayac.com>
2013-12-13 19:45:47 +0900

    update putdata

--- putdata.js
+++ putdata.js
@@ -3,8 +3,9 @@ var request = require('request');
 var fs = require('fs');
 var util = require('util');
 
-var file = __dirname + '/data.json';
-var url  = 'http://dautungoaihoipro.com/kanjiapp/index.php/dict/words';
+var file 	= __dirname + '/data.json';
+var fetch_url  	= 'http://dautungoaihoipro.com/kanjiapp/index.php/dict/words';
+var put_url		= 'http://dautungoaihoipro.com/kanjiapp/index.php/study/insert';  
  
 fs.readFile(file, 'utf8', function(err, data){
 	if (err) throw err;
@@ -12,7 +13,7 @@ fs.readFile(file, 'utf8', function(err, data){
 	data = JSON.parse(data); 	
 	_.map(data, function(words, lession_id){
 		console.log(util.format('----------%d---------', lession_id));
-		request.post(url, {form: {data: words}}, handler);
+		request.post(url, {form: {data: words.json(',')}}, handler);
 	});	
 });
 
@@ -25,6 +26,18 @@ function handler(err, res, data){
 	      console.log(msg);
 	}	
 	
-	var kanji_ids = data.kanji_ids);
+	var lession_id = data.lession_id;
+	var kanji_ids = data.kanji_ids;
 	
+	request.post(put_url, {form: { id: lession_id, data: kanji_ids }}, function(err, res, result){
+		if (err || res.statusCode != 200) 
+			throw err;
+		
+		if (result.status_code != 200){
+			console.log(util.format("there are error"));
+			return;
+		}
+
+		console.log(result.msg);
+	});
 }
